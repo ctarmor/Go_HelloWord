935fa1610f0b7c5aa5864153492de2a3584f50cf
carlosa <alvarez.carlos@hpe.com>
2021-10-02 20:39:37 -0400

    Added slice randomizer

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,33 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"1. Hi, %v. Welcome",
+		"2. Great to see you %v!",
+		"3. Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
