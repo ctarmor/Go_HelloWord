d943266dc57f32f7bba84c235ecd91626bcf0602
carlosa <alvarez.carlos@hpe.com>
2021-10-02 21:12:36 -0400

    Added multiple names
    https://golang.org/doc/tutorial/greetings-multiple-people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -7,6 +7,21 @@ import (
 	"time"
 )
 
+// Multiple names
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
 	if name == "" {
